2e7ee4dea2ce4a5e17a89dfa15628b9d235f73a3
supersuklaa <antti.sartjarvi@gmail.com>
2015-10-28 01:34:13 +0200

    send testii

--- server.js
+++ server.js
@@ -23,12 +23,12 @@ app.post('/api/webhook', function (req, res) {
     var chatid = msg['result'][0]['message']['chat']['id'];
 */
     var msg = req.body.message;
-    
+
     console.log(req.body);
 
     var data = {}
     data.chat_id = 28837686;
-    data.text = 'hi ' + ;
+    data.text = 'hi ;)';
 
     //request.post(botURL + '/sendMessage', {form: data});
 
