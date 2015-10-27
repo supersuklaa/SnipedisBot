2f3939010521f9b0b4f55102951fd1048213946c
supersuklaa <antti.sartjarvi@gmail.com>
2015-10-28 00:13:50 +0200

    saadaan viesti

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 var express = require('express');
+var request = require('request');
 var app = express();
 
 var port = process.env.PORT || 8080;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var botToken = "171954933:AAExHESfOfNnGLkjWUTuZvTiutgi7gOcTO4";
+var botURL = "https://api.telegram.org/bot" + botToken;
+
+app.get('/api/webhook', function (req, res) {
+  request(botURL + '/getupdates', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = JSON.parse(body);
+      res.send(data['result'][0]['message']['text']);
+    }
+  })
 });
 
 app.listen(port);
