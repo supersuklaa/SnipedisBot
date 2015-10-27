6b4c0828bde0202fd5d43e876c31e8415f2a594a
supersuklaa <antti.sartjarvi@gmail.com>
2015-10-27 23:43:05 +0200

    nodehommia

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 8080;
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+app.listen(port);
+console.log("App listening on port " + port);
